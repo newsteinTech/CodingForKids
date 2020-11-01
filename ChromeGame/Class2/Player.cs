38d8c95dcbe7f47537e45515ab973cdc0cc8cc04
Newstein <40591302+newsteinTech@users.noreply.github.com>
2020-11-01 20:41:04 +0530

    Add files via upload

---
+++ ChromeGame/Class2/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool isOnGround;
+    Rigidbody2D rb;
+    AudioSource audioSource;
+ 
+    public float jumpForce = 10;
+    public AudioClip jumpAudioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for isOnGround condition as well pressed Spacebar...then jump.
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true) 
+        {
+            Jump();
+        }
+
+    }
+
+
+
+    void Jump()
+    {
+        // Apply force
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        // Play jump audio
+        audioSource.PlayOneShot(jumpAudioClip);
+
+        // set isOnGround to false
+        isOnGround = false;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // set isOnGround to true
+        isOnGround = true;
+    }
+
+}
